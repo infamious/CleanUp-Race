aee8f6e40a043f1fb9d20839b4fc2327928753d9
pintoheineken <thomas.lbrn76@gmail.com>
2018-10-18 21:26:09 +0200

    Modification Voiture

--- Assets/Scripts/Player/CarController.cs
+++ Assets/Scripts/Player/CarController.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public List<AxleInfo> axleInfos; // the information about each individual axle
-    public float maxMotorTorque; // maximum torque the motor can apply to wheel
-    public float maxSteeringAngle; // maximum steer angle the wheel can have
+    [SerializeField] private WheelCollider m_FrontLeft;
+    [SerializeField] private WheelCollider m_FrontRight;
+    [SerializeField] private WheelCollider m_BackLeft;
+    [SerializeField] private WheelCollider m_BackRight;
+    [SerializeField] private float Torque;
+    [SerializeField] private int Brake = 10000;
+    [SerializeField] private float Speed;
+    [SerializeField] private float CoefAcceleration = 10f;
 
-    public void FixedUpdate()
+    public void Update()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        foreach (AxleInfo axleInfo in axleInfos)
+        Debug.Log(OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch));
+        Debug.Log(Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * Torque * CoefAcceleration * Time.deltaTime);
+
+        //Acceleration 
+
+        if(Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") > 0)
+          
+        m_BackLeft.brakeTorque = 0;
+        m_BackRight.brakeTorque = 0;
+        m_BackLeft.motorTorque = Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * Torque * CoefAcceleration * Time.deltaTime;
+        m_BackRight.motorTorque = 1000 +  Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") * Torque * CoefAcceleration * Time.deltaTime;
+
+        if(Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical") <= 0)
         {
-            if (axleInfo.steering)
-            {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
-            }
-            if (axleInfo.motor)
-            {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
-            }
+            m_BackLeft.brakeTorque = 0;
+            m_BackRight.brakeTorque = 0;
+            m_BackLeft.motorTorque = Brake* CoefAcceleration * Time.deltaTime;
+            m_BackRight.motorTorque = Brake * Torque * CoefAcceleration * Time.deltaTime;
         }
+
+
+
+        //Deceleration
+
+
+            //https://www.youtube.com/watch?v=uCM6VBS7tDo
+            //Input.GetAxis("Oculus_CrossPlatform_PrimaryThumbstickVertical")
+
     }
 }
 
-[System.Serializable]
-public class AxleInfo
-{
-    public WheelCollider leftWheel;
-    public WheelCollider rightWheel;
-    public bool motor; // is this wheel attached to motor?
-    public bool steering; // does this wheel apply steer angle?
-}
